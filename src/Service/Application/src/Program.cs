5f9332ba8da9920ca0bc22a9cd4712611f7a0a82
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2025-01-19 01:52:33 +0300

    Receive datagrams from game and log them

--- src/Service/Application/src/Program.cs
+++ src/Service/Application/src/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+const int listeningPort = 30065;
+
+using var listener = new UdpClient(listeningPort);
+
+while (true)
+{
+	var receiveResult = await listener.ReceiveAsync();
+
+	LogReceivedDatagram(receiveResult);
+
+	Console.WriteLine();
+}
+
+static void LogReceivedDatagram(UdpReceiveResult result)
+{
+	Console.WriteLine("Received {0} bytes from {1}", result.Buffer.Length, result.RemoteEndPoint);
+	Console.WriteLine(BitConverter.ToString(result.Buffer).Replace("-", ", "));
+}
