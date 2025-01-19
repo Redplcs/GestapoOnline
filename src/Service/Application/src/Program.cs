8881c084d1df2b2e191cb7ef616cbec9728d1cfe
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2025-01-20 01:12:13 +0300

    Calculate packet checksum

--- src/Service/Application/src/Program.cs
+++ src/Service/Application/src/Program.cs
@@ -2,23 +2,26 @@
 using System.Net.Sockets;
 
 const int listeningPort = 30065;
+var password = "7fas5"u8;
 
 using var listener = new UdpClient(listeningPort);
 
 while (true)
 {
-	var datagram = await listener.ReceiveAsync();
+	var datagram = listener.ReceiveAsync().GetAwaiter().GetResult();
 
 	Console.WriteLine("Received packet with {0} bytes from '{1}'.", datagram.Buffer.Length, datagram.RemoteEndPoint);
 
 	var sourcePort = PrudpVirtualPort.Deserialize(datagram.Buffer[0]);
 	var destinationPort = PrudpVirtualPort.Deserialize(datagram.Buffer[1]);
 	var (type, flags) = PrudpPacketTypeAndFlags.Deserialize(datagram.Buffer[2]);
+	var checksum = PrudpChecksum.Calculate(datagram.Buffer.AsSpan()[..^1], password);
 
 	Console.WriteLine("StreamId: {0}\tStreamType: {1}\t// Source port", sourcePort.StreamId, sourcePort.StreamType);
 	Console.WriteLine("StreamId: {0}\tStreamType: {1}\t// Destination port", destinationPort.StreamId, destinationPort.StreamType);
 	Console.WriteLine("Type: {0}", type);
 	Console.WriteLine("Flags: {0}", flags);
+	Console.WriteLine("Received checksum: {0}\tCalculated checksum: {1}", datagram.Buffer[^1], checksum);
 	Console.WriteLine("Raw: {0}", BitConverter.ToString(datagram.Buffer));
 
 	Console.WriteLine();
---
+++ src/Service/PrudpProtocol/src/PrudpChecksum.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics.Tensors;
+using System.Runtime.InteropServices;
+
+namespace Redplcs.GestapoOnline.Service.PrudpProtocol;
+
+public static class PrudpChecksum
+{
+	public static byte Calculate(ReadOnlySpan<byte> data, ReadOnlySpan<byte> password)
+	{
+		var words = MemoryMarshal.Cast<byte, uint>(data);
+		var wordsSum = TensorPrimitives.Sum(words);
+		var main = MemoryMarshal.AsBytes(MemoryMarshal.CreateReadOnlySpan(ref wordsSum, length: 1));
+		var remaining = data[^words.Length..];
+
+		var checksum = TensorPrimitives.Sum(password);
+		checksum += TensorPrimitives.Sum(remaining);
+		checksum += TensorPrimitives.Sum(main);
+
+		return checksum;
+	}
+}
